95495d21e701e3580cf6aa045b89c0da37544770
Thomas <toast@sol.at>
2020-12-10 21:34:49 +0100

    JS

---
+++ Javascript-Uebungen/js/random_color.js
@@ -0,0 +1,24 @@
+let farben = [
+    'yellow',
+    'brown',
+    'deepred',
+    'green',
+    'orange'
+];
+
+
+
+function randomColor() {
+
+    let zufallszahl = Math.floor(Math.random() * farben.length);
+
+    return farben[zufallszahl];
+}
+
+$('button.random').click(function () {
+    $('#farbe').css({
+        'background-color': randomColor()
+    });
+
+
+});
